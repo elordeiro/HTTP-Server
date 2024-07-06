3df2df18af796dacc57803bfb2213394b52c88ed
elordeiro <estevao.lordeiro@hotmail.com>
2024-07-06 17:19:14 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -6,6 +6,16 @@ import (
 	"os"
 )
 
+type Response struct {
+	Status  int
+	Headers []string
+	Message string
+}
+
+func OkResponse() []byte {
+	return []byte("HTTP/1.1 200 OK\r\n\r\n")
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -13,9 +23,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	conn.Write(OkResponse())
 }
